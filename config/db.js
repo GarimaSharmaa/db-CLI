a4303d0857b7a1aabcd79dcc4b62da8b8b65767f
Garima <sharmagarima814@gmail.com>
2021-09-13 21:20:33 +0530

    connect: mongoDb locally

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = function userdb() {
+  mongoose
+    .connect("mongodb://localhost:27017/datadb", {
+      useNewURLParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+
+    mongoose.set("findAndModify", false);
+};
