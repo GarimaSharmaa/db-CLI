fe2da06d16b607d74a16a11693caae59cadc0e1a
Garima <sharmagarima814@gmail.com>
2021-09-26 15:29:48 +0530

    remove: unwanted console logs

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
-module.exports = function userdb() {
+module.exports = function db() {
   mongoose
     .connect("mongodb://localhost:27017/datadb", {
       useNewURLParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
 };
