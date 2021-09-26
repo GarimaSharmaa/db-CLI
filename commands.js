d6b7ee437254b79a09ff0c95844cf4d7e6cde596
Garima <sharmagarima814@gmail.com>
2021-09-26 12:26:03 +0530

    feat: commander into action

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
-const { addUser, findUser } = require("../index");
+const { addUser, findUser } = require("./index");
 const program = require("commander");
 program
     .version("1.0.0")
--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const datadb = require('./config/db');
-const { db } = require('./models/dbdata');
-const dbData = require('./models/dbdata');
+const db = require("./config/db");
+const dbData = require("./models/dbdata");
 
-datadb();
+// Running mongoose locally 
+db();
 
 // Create the user
-const addUser = (user)=>{
-    dbData.create(user)
-    .then(user=>{
-        console.log("New user is added to the database");
-        db.close();
-    })
-}
+const addUser = (user) => {
+  dbData.create(user).then((user) => {
+    console.log("New user is added to the database");
+    db.close();
+  });
+};
 
-// Find the user 
-const findUser = (name)=>{
-    // make it case insensitive
-    const search = new RegExp(name, "i");
-    dbData.find({$or: [({finstname: search}, {lastname: search})]})
-    .then((user)=>{
-        console.log(user);
-        console.log(`${user.length} matches found`);
-        db.close();
-    })
-}
+// Find the user
+const findUser = (name) => {
+  // make it case insensitive
+  const search = new RegExp(name, "i");
+  dbData
+    .find({ $or: [({ finstname: search }, { lastname: search })] })
+    .then((user) => {
+      console.log(user);
+      console.log(`${user.length} matches found`);
+      db.close();
+    });
+};
 
-module.exports ={
-    addUser,
-    findUser
-}
+module.exports = {
+  addUser,
+  findUser,
+};
