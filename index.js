7035f0c9a13ad337aba47425f3c7aeec8ed1b31f
Garima <sharmagarima814@gmail.com>
2021-09-26 15:30:17 +0530

    add: all CRUD functions

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 const db = require("./config/db");
 const dbData = require("./models/dbdata");
+const mongoose = require("mongoose");
 
-// Running mongoose locally 
+// Running mongoose locally
 db();
 
 // Create the user
 const addUser = (user) => {
   dbData.create(user).then((user) => {
     console.log("New user is added to the database");
-    db.close();
+    mongoose.connection.close();
   });
 };
 
 // Find the user
 const findUser = (name) => {
-  // make it case insensitive
-  const search = new RegExp(name, "i");
+  const search = new RegExp(name, "i"); //regular expression to search
   dbData
-    .find({ $or: [({ finstname: search }, { lastname: search })] })
+    .find({ $or: [{ firstname: search }, { lastname: search }] })
     .then((user) => {
       console.log(user);
-      console.log(`${user.length} matches found`);
-      db.close();
+      console.log(`${user.length} match(es) found`);
+      mongoose.connection.close();
     });
+};
+
+// Update user
+const updateUser = (_id, user) => {
+  dbData.findByIdAndUpdate({ _id }, user).then((user) => {
+    console.log("User updated successfully");
+    mongoose.connection.close();
+  });
+};
+
+// Remove user
+const removeUser = (_id) => {
+  dbData.findByIdAndRemove({ _id }).then((user) => {
+    console.log("User removed successfully");
+    mongoose.connection.close();
+  });
+};
+
+// List users
+const listUser = () => {
+  dbData.find({}).then((user) => {
+    console.log(user);
+    console.log(`${user.length} match(es) found`);
+    mongoose.connection.close();
+  });
 };
 
 module.exports = {
   addUser,
   findUser,
+  updateUser,
+  removeUser,
+  listUser
 };
